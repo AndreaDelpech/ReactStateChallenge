08c2240b3c48e3557e69414ad4c3d6d6498eb076
Andrea Delpech <delpech.andrea@gmail.com>
2022-09-30 15:27:20 -0700

    Initial commit

---
+++ src/City.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+export default function City(props) {
+  let [temperature, setTemperature] = useState(props.temperature);
+  let [unit, setUnit] = useState("Metric");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("Imperial");
+    setTemperature(Math.round((props.temperature * 9) / 5 + 32));
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("Metric");
+    setTemperature(props.temperature);
+  }
+
+  return (
+    <div class="City">
+      It is {temperature} ({unit}){" "}
+      <a href="/" onClick={showCelsius}>
+        °C
+      </a>{" "}
+      |{" "}
+      <a href="/" onClick={showFahrenheit}>
+        °F
+      </a>{" "}
+      in {props.name}
+    </div>
+  );
+}
